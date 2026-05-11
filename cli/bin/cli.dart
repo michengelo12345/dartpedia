6fe5d33ff474d75f6e746d1163f464f38ee5ac32
guilhermeroliveira23-ctr <guilherme.r.oliveira23@aluno.senai.br>
2026-05-11 01:39:07 +0000

    Licao 03 - Tarefa 03: processamento da resposta pela requisição http

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -312,7 +312,7 @@ void printUsage() { // <-- Adicionado esta funcao na versao '0.0.3' e '0.0.4'
 	print(
 	"The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
 }
-*/
+
 
 //																|
 // ------------------------------------------------------------------------------------------------------------------------------
@@ -348,7 +348,60 @@ void printUsage() {
   );
 }
 
-// */
+*/
 // ----------------------------------------------------------------------------------------------------------------------------- |
+
+// Mudanças da tarefa 03: processamento da resposta pela requisição http
+
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+const version = '0.0.8';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  print('searchWikipedia received arguments: $arguments');
+}
+
+// tarefa 03 -->
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". '
+      'Status code: ${response.statusCode}';
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: "
+    "'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+
 
 
