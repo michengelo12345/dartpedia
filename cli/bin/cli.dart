35c890369518ba8ab83a2a9e6b296628a093845f
guilhermeroliveira23-ctr <guilherme.r.oliveira23@aluno.senai.br>
2026-05-11 02:10:11 +0000

    Licao 03 - Tarefa 04: Integração da Wikipedia API ao comando de busca

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -348,7 +348,7 @@ void printUsage() {
   );
 }
 
-*/
+
 // ----------------------------------------------------------------------------------------------------------------------------- |
 
 // Mudanças da tarefa 03: processamento da resposta pela requisição http
@@ -398,10 +398,77 @@ void printUsage() {
     "'help', 'version', 'search <ARTICLE-TITLE>'",
   );
 }
-
+*/
 
 // -----------------------------------------------------------------------------------------------------------------------------
 
+// Integração da Wikipedia API ao comando de busca
 
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
+const version = '0.0.9';
 
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void searchWikipedia(List<String>? arguments) async {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+
+    final inputFromStdin = stdin.readLineSync();
+
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return;
+    }
+
+    articleTitle = inputFromStdin;
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+
+  var articleContent = await getWikipediaArticle(articleTitle);
+
+  print(articleContent);
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". '
+      'Status code: ${response.statusCode}';
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: "
+    "'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+// */
+// -----------------------------------------------------------------------------------------------------------------------------
