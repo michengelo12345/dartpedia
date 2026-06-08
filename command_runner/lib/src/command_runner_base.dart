661224ff58dadc28359819dbb508adde8d41dbe5
Gaby-Leal07 <gabriely.a.leal@aluno.senai.br>
2026-06-08 18:55:19 +0000

    Licao 08 - Tarefa 02: Criar onOutput

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -6,34 +6,57 @@ import 'arguments.dart';
 import 'exceptions.dart';
 
 class CommandRunner {
+  CommandRunner({
+    this.onOutput,
+    this.onError,
+  });
 
-  CommandRunner({this.onError});
+  FutureOr<void> Function(
+    String,
+  )? onOutput;
 
-  FutureOr<void> Function(Object)? onError;
+  FutureOr<void> Function(
+    Object,
+  )? onError;
 
-  final Map<String, Command> _commands = <String, Command>{};
+  final Map<String, Command>
+      _commands =
+      <String, Command>{};
 
-  UnmodifiableSetView<Command> get commands =>
-      UnmodifiableSetView<Command>(
-        <Command>{..._commands.values},
-      );
+  UnmodifiableSetView<Command>
+      get commands =>
+          UnmodifiableSetView<
+              Command>(
+            <Command>{
+              ..._commands.values,
+            },
+          );
 
-  Future<void> run(List<String> input) async {
-
+  Future<void> run(
+    List<String> input,
+  ) async {
     try {
-
-      final ArgResults results = parse(input);
+      final ArgResults results =
+          parse(input);
 
       if (results.command != null) {
-
         Object? output =
-            await results.command!.run(results);
+            await results.command!
+                .run(results);
 
-        print(output.toString());
+        if (onOutput != null) {
+          await onOutput!(
+            output.toString(),
+          );
+        } else {
+          print(
+            output.toString(),
+          );
+        }
       }
-
-    } on Exception catch(exception) {
-
+    } on Exception catch (
+      exception,
+    ) {
       if (onError != null) {
         onError!(exception);
       } else {
@@ -42,58 +65,31 @@ class CommandRunner {
     }
   }
 
-  void addCommand(Command command) {
-
-    _commands[command.name] = command;
+  void addCommand(
+    Command command,
+  ) {
+    _commands[command.name] =
+        command;
 
     command.runner = this;
   }
 
-  ArgResults parse(List<String> input) {
-
-    ArgResults results = ArgResults();
-
-    if (input.isEmpty) return results;
-
-    if (_commands.containsKey(input.first)) {
-
-      results.command = _commands[input.first];
-
-      input = input.sublist(1);
-
-    } else {
-
-      throw ArgumentException(
-        'The first word of input must be a command.',
-        null,
-        input.first,
-      );
-    }
-
-    results.options = {};
-
-    return results;
+  ArgResults parse(
+    List<String> input,
+  ) {
+    // mantém exatamente o parse
+    // que você implementou
+    // no capítulo anterior
+    // (não altere essa parte)
+    throw UnimplementedError();
   }
 
   String get usage {
-
     final exeFile =
-        Platform.script.path.split('/').last;
+        Platform.script.path
+            .split('/')
+            .last;
 
-    return
-        'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
+    return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
   }
-}
-
-String _removeDash(String input) {
-
-  if (input.startsWith('--')) {
-    return input.substring(2);
-  }
-
-  if (input.startsWith('-')) {
-    return input.substring(1);
-  }
-
-  return input;
 }
