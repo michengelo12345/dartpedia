bb5148da7dd1db76210b2f44d42c286a3fb50fe7
michengelo12345 <michelangelo.neto@aluno.senai.br>
2026-06-08 18:50:36 +0000

    Licao 01 - Tarefa 08: Melhorar a saída do comando de Help

--- command_runner/lib/src/help_command.dart
+++ command_runner/lib/src/help_command.dart
@@ -1,13 +1,17 @@
 import 'dart:async';
 
-import 'arguments.dart';
+import 'package:command_runner/command_runner.dart';
+
+import 'console.dart';
+import 'exceptions.dart';
 
 class HelpCommand extends Command {
   HelpCommand() {
     addFlag(
       'verbose',
       abbr: 'v',
-      help: 'When true, this command will print each command and its options.',
+      help:
+          'When true, this command will print each command and its options.',
     );
 
     addOption(
@@ -26,16 +30,92 @@ class HelpCommand extends Command {
       'Prints usage information to the command line.';
 
   @override
-  String? get help => 'Prints this usage information';
+  String? get help =>
+      'Prints this usage information';
 
   @override
-  FutureOr<Object?> run(ArgResults args) async {
-    var usage = runner.usage;
+  FutureOr<String> run(
+    ArgResults args,
+  ) async {
+    final buffer = StringBuffer();
 
-    for (var command in runner.commands) {
-    	usage += '\n ${command.usage}';
+    buffer.writeln(
+      runner.usage.titleText,
+    );
+
+    if (args.flag('verbose')) {
+      for (var cmd in runner.commands) {
+        buffer.write(
+          _renderCommandVerbose(cmd),
+        );
+      }
+
+      return buffer.toString();
     }
 
-    return usage;
+    if (args.hasOption('command')) {
+      var (
+        :option,
+        :input,
+      ) = args.getOption('command');
+
+      var cmd =
+          runner.commands.firstWhere(
+        (command) =>
+            command.name == input,
+        orElse: () {
+          throw ArgumentException(
+            'Input ${args.commandArg} is not a known command.',
+          );
+        },
+      );
+
+      return _renderCommandVerbose(
+        cmd,
+      );
+    }
+
+    for (var command
+        in runner.commands) {
+      buffer.writeln(
+        command.usage,
+      );
+    }
+
+    return buffer.toString();
+  }
+
+  String _renderCommandVerbose(
+      Command cmd) {
+    final indent = ' ' * 10;
+
+    final buffer = StringBuffer();
+
+    buffer.writeln(
+      cmd.usage.instructionText,
+    );
+
+    buffer.writeln(
+      '$indent ${cmd.help}',
+    );
+
+    if (cmd.valueHelp != null) {
+      buffer.writeln(
+        '$indent [Argument] Required? ${cmd.requiresArgument}, Type: ${cmd.valueHelp}, Default: ${cmd.defaultValue ?? 'none'}',
+      );
+    }
+
+    buffer.writeln(
+      '$indent Options:',
+    );
+
+    for (var option
+        in cmd.options) {
+      buffer.writeln(
+        '$indent ${option.usage}',
+      );
+    }
+
+    return buffer.toString();
   }
 }
