1e23d806200f71a1453a42ef07c7f80e965136c4
guilhermeroliveira23-ctr <guilherme.r.oliveira23@aluno.senai.br>
2026-06-08 18:59:35 +0000

    Licao 08 - Tarefa 03: Usar onOutput no CLI

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -2,24 +2,31 @@ import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) {
+void main(
+  List<String> arguments,
+) {
+  var commandRunner =
+      CommandRunner(
+        onOutput:
+            (String output) async {
+          await write(output);
+        },
+        onError:
+            (Object error) {
+          if (error is Error) {
+            throw error;
+          }
 
-  var commandRunner = CommandRunner(
+          if (error is Exception) {
+            print(error);
+          }
+        },
+      )
+        ..addCommand(
+          HelpCommand(),
+        );
 
-    onError: (Object error) {
-
-      if (error is Error) {
-        throw error;
-      }
-
-      if (error is Exception) {
-        print(error);
-      }
-
-    },
-
-  )
-  ..addCommand(HelpCommand());
-
-  commandRunner.run(arguments);
+  commandRunner.run(
+    arguments,
+  );
 }
